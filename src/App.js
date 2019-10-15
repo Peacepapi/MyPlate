1b20603292fabe4be4da0af8e1b4a2a5b9405626
Pier Yos <khmaipapi@yahoo.com>
2019-10-15 12:37:39 -0400

    add bowl builder to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
+import BowlBuilder from './containers/BowlBuilder/BowlBuilder';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Test</p>
+        <BowlBuilder />
       </Layout>
     </div>
   );
