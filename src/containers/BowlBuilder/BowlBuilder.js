db31a92b2512e633bc6893e374c842646f801eea
Pier Yos <khmaipapi@yahoo.com>
2019-10-15 18:09:26 -0400

    add Add ingredient functionality to BowlBuilder

--- src/containers/BowlBuilder/BowlBuilder.js
+++ src/containers/BowlBuilder/BowlBuilder.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Bowl from '../../components/Bowl/Bowl';
+import BuildControls from '../../components/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+    fillings: 2,
+    rice: 1,
+    beans: 1,
+    cheese: 0.5
+}
 
 class BowlBuilder extends Component {
     state = {
@@ -9,13 +17,31 @@ class BowlBuilder extends Component {
             rice: 0,
             beans: 2,
             cheese: 2
-        }
+        },
+        totalPrices: 4
     }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldprice = this.state.totalPrices;
+        const newPrice = oldprice + priceAddition;
+        this.setState({totalPrices: newPrice, ingredients: updatedIngredients});
+    }
+
     render() {
         return (
             <Aux>
                 <Bowl ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls 
+                    ingredientAdded={this.addIngredientHandler}
+                    ingredients={this.state.ingredients}
+                />
             </Aux>
         );
     }
