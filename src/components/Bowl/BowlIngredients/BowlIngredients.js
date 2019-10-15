502fc7f40f5c151aa2082e6d8cff9cf3de34c7c6
Pier Yos <khmaipapi@yahoo.com>
2019-10-15 12:56:52 -0400

    change comp to class and add prop types validation

--- src/components/Bowl/BowlIngredients/BowlIngredients.js
+++ src/components/Bowl/BowlIngredients/BowlIngredients.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './BowlIngredient.css';
+import PropTypes from 'prop-types';
 
-const BowlIngredient = (props) => {
-    let ingredient = null;
+class BowlIngredient extends Component {
+    render() {
+        let ingredient = null;
     
-    switch (props.type) {
-        case ('fillings'):
-            ingredient = <div className={classes.Fillings}></div>;
-            break;
-        case ('rice'):
-            ingredient = <div className={classes.Rice}></div>;
-            break;
-        case ('beans'):
-            ingredient = <div className={classes.Beans}></div>;
-            break;
-        case ('toppings'):
-            ingredient = <div className={classes.Toppings}></div>;
-            break;
-        default:
-            ingredient = null;
+        switch (this.props.type) {
+            case ('fillings'):
+                ingredient = <div className={classes.Fillings}></div>;
+                break;
+            case ('rice'):
+                ingredient = <div className={classes.Rice}></div>;
+                break;
+            case ('beans'):
+                ingredient = <div className={classes.Beans}></div>;
+                break;
+            case ('toppings'):
+                ingredient = <div className={classes.Toppings}></div>;
+                break;
+            default:
+                ingredient = null;
+        }
+        return ingredient;
     }
-    return ingredient;
+}
+
+BowlIngredient.propTypes = {
+    type: PropTypes.string.isRequired
 }
 
 export default BowlIngredient;
