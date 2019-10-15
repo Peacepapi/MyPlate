003b6ffdd880295b26ba18e55e1fcae875490364
Pier Yos <khmaipapi@yahoo.com>
2019-10-15 15:59:42 -0400

    add bowl to bowlbuilder

--- src/containers/BowlBuilder/BowlBuilder.js
+++ src/containers/BowlBuilder/BowlBuilder.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Bowl from '../../components/Bowl/Bowl';
 
 class BowlBuilder extends Component {
+    state = {
+        ingredients: {
+            fillings: 0,
+            rice: 0,
+            beans: 2,
+            cheese: 2
+        }
+    }
     render() {
         return (
             <Aux>
-                <div>Bowl</div>
+                <Bowl ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
