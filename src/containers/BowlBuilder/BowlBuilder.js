5f8c809ff55d03125b4a6ec7b50cb3c7f3d37a36
Pier Yos <khmaipapi@yahoo.com>
2019-10-15 12:37:29 -0400

    create bowl builder component as stateful

---
+++ src/containers/BowlBuilder/BowlBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BowlBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Bowl</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BowlBuilder;
+
