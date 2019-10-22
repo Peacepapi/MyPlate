4971e8b6f0e353e9c36adcf0061c7417bf1be944
Pier Yos <khmaipapi@yahoo.com>
2019-10-22 11:32:01 -0400

    change to class comp for lifecycle use

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    return (
-        <Aux>
-            <Backdrop show={props.showModal} clicked={props.modalClosed}/>
-            <div className={classes.Modal}
-                style={{
-                    transform: props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
-                    opacity: props.showModal ? '1':'0'
-                }}
-            >
-                {props.children}
-            </div>
-        </Aux>
-        
-    )
+class modal extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.showModal !== this.props.showModal;
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Backdrop show={this.props.showModal} clicked={this.props.modalClosed}/>
+                <div className={classes.Modal}
+                    style={{
+                        transform: this.props.showModal ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.showModal ? '1':'0'
+                    }}
+                >
+                    {this.props.children}
+                </div>
+            </Aux> 
+        )
+    }
+    
 }
 
 export default modal;
